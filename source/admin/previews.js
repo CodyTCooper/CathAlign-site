4c389a8299b6a7666ba19992869e2ecad501e57c
CodyTCooper <38065584+CodyTCooper@users.noreply.github.com>
2019-07-16 22:06:06 -0700

    Update previews.js

--- source/admin/previews.js
+++ source/admin/previews.js
@@ -29,12 +29,6 @@ const ProductPreview = ({ entry, widgetFor }) => {
       <div className="content">
         <div className="meta">
           <h1>{data.title}</h1>
-          <div className="weight">
-            <span>{`${data.weight}kg`}</span>
-          </div>
-          <div className="price">
-            <span>{`€${data.price}`}</span>
-          </div>
         </div>
         <div className="body">{widgetFor('body')}</div>
       </div>
